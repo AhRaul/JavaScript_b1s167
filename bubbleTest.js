5e20646f80d9f8115160f39a61712982a493ef31
RA <relian.skype@yandex.ru>
2025-06-23 13:43:18 +0300

    Разделил задачи на отдельные функции.

--- bubbleTest.js
+++ bubbleTest.js
@@ -4,23 +4,40 @@ var scores = 	[60, 50, 60, 58, 54, 54,
 				 69, 64, 66, 55, 52, 61,
 				 46, 31, 57, 52, 44, 18,
 				 41, 53, 55, 61, 51, 44];
-var highestScore = 0;
-var bestSolutions = [];
-var i=0;
-while (i < scores.length){
-	console.log("Bubble solution #" + i + " score: " + scores[i]);
-	i++;
-	if (highestScore < scores[i]) {
-		highestScore = scores[i]
+				 
+function printAllExperements(scores) {
+	var i=0;
+	while (i < scores.length){
+		console.log("Bubble solution #" + i + " score: " + scores[i]);
+		i++;
 	}
+	console.log("Bubble tests: " + scores.length);
 }
-console.log("Bubble tests: " + scores.length);
-console.log("Highest bubble score: " + highestScore);
-
-for(var i2 = 0; i2 < scores.length; i2++) {
-	if (scores[i2] == highestScore) {
-		bestSolutions.push(i2);
+				
+function printBestResult(scores) {
+	var highestScore = 0;
+	var i=0;
+	while (i < scores.length){
+		i++;
+		if (highestScore < scores[i]) {
+			highestScore = scores[i]
+		}
 	}
+	console.log("Highest bubble score: " + highestScore);
+	return highestScore;
+}	
+
+function printBestExperimentsNumbers(scores) {
+	var bestSolutions = [];
+	var highestScore = printBestResult(scores);
+	for(var i2 = 0; i2 < scores.length; i2++) {
+		if (scores[i2] == highestScore) {
+			bestSolutions.push(i2);
+		}
+	}
+	console.log("Highest bubble score: " + bestSolutions);
 }
 
-console.log("Highest bubble score: " + bestSolutions);
+printAllExperements(scores);
+printBestResult(scores);
+printBestExperimentsNumbers(scores);
